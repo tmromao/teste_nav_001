93bc24557c8c1302a73660f66ee7752fceaf2401
tromao <tiagomromao@gmail.com>
2022-04-02 12:24:31 +0100

    Added 3 objects, screen, for the bottomscreen

---
+++ app/src/main/java/com/example/teste_nav_001/ui/theme/BottomBarScreen.kt
@@ -0,0 +1,32 @@
+package com.example.teste_nav_001.ui.theme
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomBarScreen(
+    val route : String,
+    val title: String,
+    val icon: ImageVector
+){
+    object Home: BottomBarScreen(
+        route = "home",
+        title = "Home",
+        icon = Icons.Default.Home
+    )
+
+    object Profile: BottomBarScreen(
+        route = "profile",
+        title = "Profile",
+        icon = Icons.Default.Person
+    )
+
+    object Settings: BottomBarScreen(
+        route = "settings",
+        title = "Settings",
+        icon = Icons.Default.Settings
+    )
+
+}
